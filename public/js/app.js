2788cb3149252462cd7aecd79d240e1efee9c158
= <=>
2024-03-08 20:40:56 +0530

    Designed tools

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,21 @@
-let switchList = ['.lg-logo-back', '.lg-logo', '.logo']
+let switchList = [".lg-logo-back", ".lg-logo", ".logo"];
 
 switchList.forEach((s) => {
-$(s).on('click', (e) => {
-    console.log("clicked")
-    $(".lg-logo-back").toggleClass('disabled');
-    $(".lg-logo").toggleClass('disabled');
-})
+    $(s).on("click", (e) => {
+        console.log("clicked");
+        $(".lg-logo-back").toggleClass("disabled");
+        $(".lg-logo").toggleClass("disabled");
+    });
 });
 
+$(".drop-down").each(function (index, dropdown) {
+    console.log(this);
+    $(dropdown).on("click", function (e) {
+        $(".drop-down").each(function (Index, Dropdown) {
+            if (index != Index) {
+                $(Dropdown).removeClass("open");
+            }
+        });
+        $(this).toggleClass("open");
+    });
+});
